9b0ba6bdb71e0a99c91685a61fd5b757993124ae
Tomasz Stęczniewski <tomaszsteczniewski@gmail.com>
2015-03-27 16:43:45 +0100

    Parallel fit.

--- src/main/scala/Algorithm.scala
+++ src/main/scala/Algorithm.scala
@@ -12,6 +12,7 @@ import org.deeplearning4j.models.rnn.Tree
 import org.deeplearning4j.models.word2vec.wordstore.VocabCache
 import org.deeplearning4j.spark.models.word2vec.{Word2Vec => SparkWord2Vec}
 import org.deeplearning4j.text.corpora.treeparser.TreeVectorizer
+import org.nd4j.linalg.api.ndarray.INDArray
 
 import scala.collection.JavaConversions._
 
@@ -31,30 +32,54 @@ class Algorithm(val ap: AlgorithmParams)
       val result = new SparkWord2Vec().train(data.phrases)
       (result.getFirst, result.getSecond)
     }
-    val rawTreess = data.labeledPhrases.mapPartitions(labeledPhrases => {
+    val rawTrees = data.labeledPhrases.mapPartitions(labeledPhrases => {
       val treeVectorizer = new TreeVectorizer()
       labeledPhrases.map(labeledPhrase => (treeVectorizer.getTreesWithLabels(labeledPhrase.phrase, data.labels), labeledPhrase.sentiment))
     })
-    val convertedTreess = rawTreess.map(rawTrees => {
-      val (rawTrees_, sentiment) = rawTrees
-      rawTrees_.map(rawTree => new Pair[Tree, Integer](Tree.fromTreeVectorizer(rawTree, vocabCache, weightLookupTable), sentiment))
+    val convertedTrees = rawTrees.flatMap(rawTreesWithSentiment => {
+      val (rawTrees, sentiment) = rawTreesWithSentiment
+      rawTrees.map(rawTree => new Pair[Tree, Integer](Tree.fromTreeVectorizer(rawTree, vocabCache, weightLookupTable), sentiment))
     })
-    val convertedTrees = convertedTreess.reduce(_ ++ _)
-    val rnnSettings = new RNN.Settings(ap.inSize, data.labels.length)
-    val rnn = new RNN(rnnSettings)
-    rnn.fit(convertedTrees)
+    val collectedConvertedTrees = convertedTrees.glom()
+    val judgesAndCombinators = collectedConvertedTrees.map(convertedTrees => {
+      val rnnSettings = new RNN.Settings(ap.inSize, data.labels.length)
+      val rnn = new RNN(rnnSettings)
+      val convertedTreesList = convertedTrees.toList
+      var lastError = 0.0
+      var error = rnn.error(convertedTreesList)
+      do {
+        rnn.fit(convertedTrees.toList)
+        lastError = error
+        error = rnn.error(convertedTreesList)
+      } while(error < lastError)
+      (rnn.judge, rnn.combinator, 1.0)
+    })
+    val (judge, combinator, _) = judgesAndCombinators.reduce({
+      case ((jl, cl, ql), (jr, cr, qr)) =>
+        val sum = ql + qr
+        val jlq = jl.mul(ql)
+        val jrq = jr.mul(qr)
+        val j = (jlq.add(jrq)).mul(1.0 / sum)
+        val clq = cl.mul(ql)
+        val crq = cr.mul(qr)
+        val c = (clq.add(crq)).mul(1.0 / sum)
+        (j, c, sum)
+    })
     new Model(
       vocabCache = vocabCache,
       weightLookupTable = weightLookupTable,
-      rnn = rnn,
+      judge = judge,
+      combinator = combinator,
       labels = data.labels
     )
   }
 
   def predict(model: Model, query: Query): PredictedResult = {
     val rawTrees = new TreeVectorizer().getTreesWithLabels(query.content, model.labels)
+    val rnnSettings = new RNN.Settings(ap.inSize, model.labels.length)
+    val rnn = new RNN(rnnSettings, model.combinator, model.judge)
     val convertedTrees = rawTrees.map(Tree.fromTreeVectorizer(_, model.vocabCache, model.weightLookupTable))
-    val sentiments = convertedTrees.map(model.rnn.predictClass(_))
+    val sentiments = convertedTrees.map(rnn.predictClass(_))
     PredictedResult(sentiments = sentiments.toList)
   }
 }
@@ -62,6 +87,7 @@ class Algorithm(val ap: AlgorithmParams)
 class Model(
   val vocabCache: VocabCache,
   val weightLookupTable: WeightLookupTable,
-  val rnn: RNN,
+  val judge: INDArray,
+  val combinator: INDArray,
   val labels: List[String]
 ) extends Serializable
