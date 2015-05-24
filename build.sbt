eda05f475533681fdf860f678fd0497d6ec2e296
Tomasz Steczniewski <tomaszsteczniewski@gmail.com>
2015-05-24 00:06:36 -0700

    New version of prediction io.

--- build.sbt
+++ build.sbt
@@ -5,11 +5,11 @@ name := "template-scala-rnn"
 organization := "io.prediction"
 
 libraryDependencies ++= Seq(
-  "io.prediction"      %% "core"           % "0.9.0"   % "provided",
-  "org.apache.spark"   %% "spark-core"     % "1.2.0"   % "provided",
-  "org.apache.spark"   %% "spark-mllib"    % "1.2.0"   % "provided",
+  "io.prediction"      %% "core"           % pioVersion.value   % "provided",
+  "org.apache.spark"   %% "spark-core"     % "1.3.0"            % "provided",
+  "org.apache.spark"   %% "spark-mllib"    % "1.3.0"            % "provided",
   "org.scalanlp"       %% "breeze"         % "0.11.2",
   "org.scalanlp"       %% "breeze-natives" % "0.11.2",
   "org.apache.opennlp" %  "opennlp-tools"  % "1.5.3",
-  "org.scalatest" % "scalatest_2.10" % "latest.integration" % "test"
+  "org.scalatest"      % "scalatest_2.10"  % "latest.integration" % "test"
 )
--- src/main/scala/DataSource.scala
+++ src/main/scala/DataSource.scala
@@ -1,53 +1,52 @@
 package org.template.vanilla
 
 import io.prediction.controller._
-import io.prediction.data.storage.Storage
+import io.prediction.data.store.PEventStore
 
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
 import grizzled.slf4j.Logger
 
-case class DataSourceParams(appId: Int) extends Params
+case class DataSourceParams(appName: String) extends Params
 
 class DataSource(val dsp: DataSourceParams)
   extends PDataSource[TrainingData,
-      EmptyEvaluationInfo, Query, EmptyActualResult] {
+    EmptyEvaluationInfo, Query, EmptyActualResult] {
 
   @transient lazy val logger = Logger[this.type]
 
   override
   def readTraining(sc: SparkContext): TrainingData = {
-    val eventsDb = Storage.getPEvents()
-    val eventsRDD: RDD[LabeledPhrase] = eventsDb
+    val eventsRDD: RDD[LabeledPhrase] = PEventStore
       .aggregateProperties(
-        appId = dsp.appId,
+        appName = dsp.appName,
         entityType = "phrase",
         required = Some(List("sentenceId", "phrase", "sentiment")))(sc)
       .map({
-        case (entityId, properties) =>
-          LabeledPhrase(
-            phraseId = entityId.toInt,
-            sentenceId = properties.get[String]("sentenceId").toInt,
-            phrase = properties.get[String]("phrase"),
-            sentiment = properties.get[String]("sentiment").toInt
-          )
-      })
+      case (entityId, properties) =>
+        LabeledPhrase(
+          phraseId = entityId.toInt,
+          sentenceId = properties.get[String]("sentenceId").toInt,
+          phrase = properties.get[String]("phrase"),
+          sentiment = properties.get[String]("sentiment").toInt
+        )
+    })
 
     new TrainingData(eventsRDD)
   }
 }
 
 case class LabeledPhrase(
-  phraseId: Int,
-  sentenceId: Int,
-  phrase: String,
-  sentiment: Int
-)
+                          phraseId: Int,
+                          sentenceId: Int,
+                          phrase: String,
+                          sentiment: Int
+                          )
 
 class TrainingData(
-  val labeledPhrases: RDD[LabeledPhrase]
-) extends Serializable with SanityCheck {
+                    val labeledPhrases: RDD[LabeledPhrase]
+                    ) extends Serializable with SanityCheck {
   override def toString = {
     s"events: [${labeledPhrases.count()}] (${labeledPhrases.take(2).toList}...)"
   }
